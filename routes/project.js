fe202fda39bcae10261d11cdbaf23ba839e0b982
George Ansong <Georgeansong7@gmail.com>
2021-07-21 00:25:43 +0000

    minor changes and fixes

--- routes/project.js
+++ routes/project.js
@@ -7,11 +7,13 @@ const {
   updateProject,
   addDevToProject,
   removeUserFromProject,
+  getProjects,
 } = require("../controllers/project");
 
 const router = express.Router();
 
 router.post("/createProject/:id", createProject);
+router.get("/getprojects/:userID", getProjects);
 router.get("/getProject/:id", getProjectDetails);
 router.delete("/deleteProject/:id", deleteProject);
 router.post("/inviteToProject", addDevToProject);
